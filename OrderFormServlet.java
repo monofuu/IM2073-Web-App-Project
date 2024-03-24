f88fd6c197129fd010181ad74b2f79bb59708fcb
shixwase <70529239+shixwase@users.noreply.github.com>
2024-03-25 02:26:13 +0800

    Create OrderFormServlet.java

---
+++ OrderFormServlet.java
@@ -0,0 +1,74 @@
+// To save as "ebookshop\WEB-INF\classes\QueryServlet.java".
+import java.io.*;
+import java.sql.*;
+import jakarta.servlet.*;            // Tomcat 10
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+//import javax.servlet.*;            // Tomcat 9
+//import javax.servlet.http.*;
+//import javax.servlet.annotation.*;
+
+@WebServlet("/adoptionqueryresults")   // Configure the request URL for this servlet (Tomcat 7/Servlet 3.0 upwards)
+public class OrderFormServlet extends HttpServlet {
+
+   // The doGet() runs once per HTTP GET request to this servlet.
+   @Override
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+               throws ServletException, IOException {
+      // Set the MIME type for the response message
+      response.setContentType("text/html");
+      // Get a output writer to write the response message into the network socket
+      PrintWriter out = response.getWriter();
+      // Print an HTML page as the output of the query
+      out.println("<!DOCTYPE html>");
+      out.println("<html>");
+      out.println("<head><title>Query Response</title></head>");
+      out.println("<body>");
+
+      try (
+         // Step 1: Allocate a database 'Connection' object
+         Connection conn = DriverManager.getConnection(
+               "jdbc:mysql://localhost:3306/ebookshop?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+               "myuser", "xxxx");   // For MySQL
+               // The format is: "jdbc:mysql://hostname:port/databaseName", "username", "password"
+
+         // Step 2: Allocate a 'Statement' object in the Connection
+         Statement stmt = conn.createStatement();
+      ) {
+         // Step 3: Execute a SQL SELECT query
+         // === Form the SQL command - BEGIN ===
+         String sqlStr = "select * from books where author = "
+               + "'" + request.getParameterValues("type") + "'"   // Single-quote SQL string
+               + " and qty > 0 order by price desc";
+         // === Form the SQL command - END ===
+
+         out.println("<h3>Thank you for your query.</h3>");
+         out.println("<p>Your SQL statement is: " + sqlStr + "</p>"); // Echo for debugging
+         ResultSet rset = stmt.executeQuery(sqlStr);  // Send the query to the server
+
+         // Step 4: Process the query result set
+         out.println("<form method='get' action='eshoporder'>");
+         
+         // For each row in ResultSet, print one checkbox inside the <form>
+         while(rset.next()) {
+            out.println("<p><input type='checkbox' name='id' value="
+                  + "'" + rset.getString("id") + "' />"
+                  + rset.getString("name") + ", "
+                  + rset.getString("type") + ", $"
+                  + rset.getString("price") + "</p>");
+         }
+ 
+         // Print the submit button and </form> end-tag
+         out.println("<p><input type='submit' value='ORDER' />");
+         out.println("</form>");
+         // === Step 4 ends HERE - Do NOT delete the following codes ===
+      } catch(SQLException ex) {
+         out.println("<p>Error: " + ex.getMessage() + "</p>");
+         out.println("<p>Check Tomcat console for details.</p>");
+         ex.printStackTrace();
+      }  // Step 5: Close conn and stmt - Done automatically by try-with-resources (JDK 7)
+ 
+      out.println("</body></html>");
+      out.close();
+   }
+}
