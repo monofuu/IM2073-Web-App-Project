7c9383e6797ba66edc22fbac9f391a03dd966870
fuu <162871021+monofuu@users.noreply.github.com>
2024-07-29 12:49:41 +0800

    Update QueryServlet.java

--- QueryServlet.java
+++ QueryServlet.java
@@ -1,13 +1,10 @@
 import java.io.*;
 import java.sql.*;
-import jakarta.servlet.*;            // Tomcat 10
+import jakarta.servlet.*;          
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
-//import javax.servlet.*;            // Tomcat 9
-//import javax.servlet.http.*;
-//import javax.servlet.annotation.*;
 
-@WebServlet("/petquery")   // Configure the request URL for this servlet (Tomcat 7/Servlet 3.0 upwards)
+@WebServlet("/petquery") 
 public class QueryServlet extends HttpServlet {
 
    // The doGet() runs once per HTTP GET request to this servlet.
@@ -25,19 +22,15 @@ public class QueryServlet extends HttpServlet {
       out.println("<body>");
 
       try (
-         // Step 1: Allocate a database 'Connection' object
          Connection conn = DriverManager.getConnection(
                "jdbc:mysql://localhost:3306/petdb?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
                "myuser", "xxxx");   // For MySQL
                // The format is: "jdbc:mysql://hostname:port/databaseName", "username", "password"
 
-         // Step 2: Allocate a 'Statement' object in the Connection
          Statement stmt = conn.createStatement();
       ) {
-         // Step 3: Execute a SQL SELECT query
-         // === Form the SQL command - BEGIN ===
          String[] authors = request.getParameterValues("type");  // Returns an array of Strings
-         String sqlStr = "SELECT * FROM class101 WHERE type IN (";
+         String sqlStr = "SELECT * FROM petdb WHERE type IN (";
          for (int i = 0; i < types.length; ++i) {
             if (i < types.length - 1) {
                sqlStr += "'" + types[i] + "', ";  // need a commas
@@ -46,13 +39,11 @@ public class QueryServlet extends HttpServlet {
             }
          }
          sqlStr += ") AND qty > 0 ORDER BY author ASC, title ASC";
-         // === Form the SQL command - END ===
 
          out.println("<h3>Thank you for your query.</h3>");
          out.println("<p>Your SQL statement is: " + sqlStr + "</p>"); // Echo for debugging
          ResultSet rset = stmt.executeQuery(sqlStr);  // Send the query to the server
 
-         // Step 4: Process the query result set
          int count = 0;
          while(rset.next()) {
             // Print a paragraph <p>...</p> for each record
@@ -62,12 +53,12 @@ public class QueryServlet extends HttpServlet {
             count++;
          }
          out.println("<p>==== " + count + " records found =====</p>");
-         // === Step 4 ends HERE - Do NOT delete the following codes ===
+
       } catch(SQLException ex) {
          out.println("<p>Error: " + ex.getMessage() + "</p>");
          out.println("<p>Check Tomcat console for details.</p>");
          ex.printStackTrace();
-      }  // Step 5: Close conn and stmt - Done automatically by try-with-resources (JDK 7)
+      } 
  
       out.println("</body></html>");
       out.close();
